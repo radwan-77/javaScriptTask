e5a3e4896d2f3b2c4aa2db8bb8e3266e68b72a91
RADWAN <radhwan_3682@limu.edu.ly>
2025-11-25 01:38:38 +0200

    feat: Add initial structure for product and category management with placeholder functions and initial calls.

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-// afad
+let products = [];
+
+async function fetchProducts() {
+    // TODO: fetch products
+}
+
+async function fetchCategories() {
+    // TODO: fetch categories
+}
+
+function renderProducts(list) {
+    // TODO: loop and show cards
+}
+
+function handleSearch() {
+    // TODO: live search
+}
+
+function handleCategoryFilter() {
+    // TODO: filter by category
+}
+
+function showDetails(product) {
+    // TODO: fill modal and open it
+}
+
+// Init
+fetchProducts();
+fetchCategories();
